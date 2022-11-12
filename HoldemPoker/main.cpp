e78e0948950ed2beae4d8b62d4aa715c2b1a0dd5
cemkupeli <63188941+cemkupeli@users.noreply.github.com>
2022-11-12 15:38:57 -0500

    Initial Commit

---
+++ HoldemPoker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HoldemPoker
+//
+//  Created by Cem Kupeli on 11/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
